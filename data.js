e88064e8aff43668b3d7a27594832e81340a23ac
Dskilushang <danielkilusisa@gmail.com>
2026-09-07 23:26:44 +0100

    Create data.js

---
+++ data.js
@@ -0,0 +1,38 @@
+// data.js
+const data = {
+  membres: [
+    { nom: 'Jean M.', departement: 'Chorale' },
+    { nom: 'Marie K.', departement: 'Finances' },
+    { nom: 'Pierre N.', departement: 'PAGAS' },
+    { nom: 'Lucie T.', departement: 'Conseil des sages' },
+    { nom: 'Paul R.', departement: 'Chorale' },
+    { nom: 'Anne S.', departement: 'PAGAS' },
+    { nom: 'David L.', departement: 'Finances' },
+    { nom: 'Sarah B.', departement: 'Conseil des sages' }
+  ],
+  activites: [
+    { jour: 'MER', heure: '09h00 - 13h00', titre: 'Encontro das Mamães' },
+    { jour: 'JEU', heure: '09h00 - 16h00', titre: 'Consulta Profética' },
+    { jour: 'VEN', heure: '16h00 - 18h00', titre: 'Culto de Oração e Libertação' },
+    { jour: 'DIM', heure: '16h00 - 18h00', titre: 'Culto de Louvor e Adoração' }
+  ],
+  rapports: [
+    'Rapport Chorale – Avril 2025',
+    'Rapport Intercession – Avril 2025',
+    'Rapport Finances – Avril 2025',
+    'Rapport Action Sociale – Avril 2025',
+    'Rapport Jeunesse – Avril 2025'
+  ],
+  departements: ['PAGAS', 'Chorale', 'Finances', 'Conseil des sages', 'Jeunesse', 'Action Sociale', 'Intercession'],
+  annonces: [
+    'Réunion des sages le 15 mai',
+    'Collecte spéciale pour les orphelins',
+    'Retraite spirituelle du 20 au 22 juin'
+  ],
+  statistiques: {
+    membresTotaux: 8,
+    activitesCeMois: 4,
+    rapportsEnAttente: 2,
+    annoncesActives: 3
+  }
+};
